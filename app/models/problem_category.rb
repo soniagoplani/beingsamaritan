59cce629b2f45d7e181315077a1c4fe8bde0c291
sonia02goplani <sonia.goplani779@gmail.com>
2017-08-01 16:57:19 +0530

    delete

--- app/models/problem_category.rb
+++ app/models/problem_category.rb
@@ -1,5 +1,5 @@
 class ProblemCategory < ActiveRecord::Base
-has_many :problem_posts , class_name: "ProblemPost"
+has_many :problem_posts , class_name: "ProblemPost", :primary_key => :id ,:dependent :destroy
 validates :category_id , presence: true
   validates :name_category , presence: true , length: { maximum: 255}
 end
